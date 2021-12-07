97620c381b496249c0f20703b02de0c56d9e458a
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 03:56:22 +0700

    Add files via upload

---
+++ 2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите три целых числа: ");
+            string[] s = Console.ReadLine().Split();
+            int x1 = int.Parse(s[0]);
+            int x2 = int.Parse(s[1]);
+            int x3 = int.Parse(s[2]);
+            if( 1 <= x1 && x1<= 3)
+            {
+                Console.WriteLine($"{x1} принадлежит интервалу [1; 3];");
+            }
+            if (1 <= x2 && x2 <= 3)
+            {
+                Console.WriteLine($"{x2} принадлежит интервалу [1; 3];");
+            }
+            if (1 <= x3 && x3 <= 3)
+            {
+                Console.WriteLine($"{x3} принадлежит интервалу [1; 3];");
+            }
+        }
+    }
+}
