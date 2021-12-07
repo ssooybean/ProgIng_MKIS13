42d52b8a889afa06b0ee0fc672e515ba8d1b5823
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 06:09:31 +0700

    Add files via upload

---
+++ 9.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i < 7; i++)
+            {
+                for (int j =  1; j <= 10; j++)
+                {
+                    Console.WriteLine($"{i} x {j} = {i * j}");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
