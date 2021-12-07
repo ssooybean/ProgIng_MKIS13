982fc49e89fc7ba3fb3f313af7135acf87d37877
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 05:06:07 +0700

    Add files via upload

---
+++ 6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("p = ");
+            int p = Convert.ToInt32(Console.ReadLine());
+            int i = p;
+            int factorial = 1;
+            do
+            {
+                factorial = factorial * i;
+                --i;
+            }
+            while (i > 1);
+
+            Console.WriteLine($"{p}!= {factorial}");
+            Console.ReadLine();
+        }
+    }
+}
