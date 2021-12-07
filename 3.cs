27d6b563981e9e9bfc18fcd8173472a706f83ff7
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 04:02:57 +0700

    Add files via upload

---
+++ 3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите одно целое число: ");
+            int a = int.Parse(Console.ReadLine());
+            if (a < 0)
+            {
+                Console.WriteLine($"{a} меньше нуля.");
+                return;
+            }
+            if (a > 0)
+            {
+                Console.WriteLine($"{a} больше нуля.");
+            }
+            else
+            {
+                Console.WriteLine($"{a} равно нулю.");
+            }
+        }
+    }
+}
