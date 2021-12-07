7d364aa6b07ca00b3ce2d347149704d05c766ffb
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 05:44:23 +0700

    Add files via upload

---
+++ 7.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("N = ");
+            int n = int.Parse(Console.ReadLine());
+            int summ = 0;
+            int coast = 0;
+            for (int i = 3; i < n - 1; i+= 2)
+            {
+                coast += 2;
+                Console.WriteLine($"{i} {i + 2}");
+                summ += i + i + 2;
+            }
+            Console.WriteLine($"Сумма: {summ}, количество: {coast}");
+        }
+    }
+}
