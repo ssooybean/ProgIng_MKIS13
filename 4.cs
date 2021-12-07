39db329b5d1f49eadebc4e95702dd5f5cf04f94d
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 04:24:32 +0700

    Add files via upload

---
+++ 4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите стоимость покупки: ");
+            double cost = double.Parse(Console.ReadLine());
+            if (cost <= 500)
+            {
+                Console.WriteLine($"Стоимость покупки {cost}.");
+                return;
+            }
+            if (cost > 500 && cost <= 1000)
+            {
+                Console.WriteLine($"Стоимость покупки {cost - cost * 0.03}.");
+            }
+            else
+            {
+                Console.WriteLine($"Стоимость покупки {cost - cost * 0.05}.");
+            }
+        }
+    }
+}
