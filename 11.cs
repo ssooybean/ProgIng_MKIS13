33d60563c57b17ccf4594914b3c1804103d0a1b0
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 06:14:58 +0700

    Add files via upload

---
+++ 11.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            var array = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+
+            for (int i = 1; i < n; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
