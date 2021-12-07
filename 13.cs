b7c01654331f7559d0f3ad0f3aaa3e7cc5e4ecc7
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 06:24:27 +0700

    Add files via upload

---
+++ 13.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+            var p = Array.ConvertAll(Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries), s => int.Parse(s)).ToList();
+            var min = p.Min();
+            var max = p.Max();
+            var res = p.Select(x => x == max ? min : x == min ? max : x);
+            Console.WriteLine(string.Join(" ", res));
+        }
+    }
+}
