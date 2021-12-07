714d320ffd275482cec9b2b25676da095b2aa273
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 06:01:55 +0700

    Add files via upload

---
+++ 8.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("M = ");
+            int m = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <= m; i++)
+            {
+                int j = 0;
+                while (j < i)
+                {
+                    Console.Write("*");
+                    j++;
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
