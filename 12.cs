9289c2c80479bb36918d4e755ae0e4452f1e89b4
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 06:20:33 +0700

    Add files via upload

---
+++ 12.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество чисел в последовательности: ");
+            var n = int.Parse(Console.ReadLine());
+            var p = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+            var count = 0;
+
+            for (var i = 1; i < n - 1; i++)
+            {
+                if (p[i - 1] % 2 == 0 && p[i + 1] % 2 == 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
