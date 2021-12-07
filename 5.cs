6a09361bfc28e0c72f6033f05c017fcbc1093dde
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 04:41:41 +0700

    Add files via upload

---
+++ 5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Через пробел задайте первый член арифмитической прогрессии, шаг и количество искомых членов: ");
+            string[] st = Console.ReadLine().Split();
+            double first = double.Parse(st[0]);
+            double d = double.Parse(st[1]);
+            int kolvo = int.Parse(st[2]);
+            double sumpr = 0;
+            for(int i = 0; i < kolvo; i++)
+            {
+                sumpr += first + d * i;
+            }
+            Console.WriteLine($"Сумма первых членов заданной арифмитической прогрессии равна {sumpr}.");
+        }
+    }
+}
