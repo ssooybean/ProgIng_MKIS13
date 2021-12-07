d70046ef9160b3f2bc39caad07a3ce0aaa98ebbe
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 06:13:11 +0700

    Add files via upload

---
+++ 10.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            var array = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+
+            int max = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (max < array[i])
+                {
+                    max = array[i];
+                }
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
