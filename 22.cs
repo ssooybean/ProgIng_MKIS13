058d84eaad7c424935ac060f37fb61cde630b885
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 20:09:05 +0700

    Add files via upload

---
+++ 22.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите русскоязычную строку:\t");
+            string s = Console.ReadLine();
+            int count = 0;
+            for(int i = 0; i < s.Length; i++)
+            {
+                if (s[i]== ' ' || s[i] == '.')
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Количество слов в тексте равно {count}.");
+        }
+    }
+}
