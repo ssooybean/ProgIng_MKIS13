8646c4da7cae89d453a0a1993657219c0b7f2c6a
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 19:22:26 +0700

    Add files via upload

---
+++ 15.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размерность массива:\t");
+            int n = int.Parse(Console.ReadLine());
+            int[] MyArray = new int[n];
+            int count = 0;
+            Console.WriteLine($"Введите {n} целых чисел (после каждого нажмите Shift).");
+            for (int i = 0; i < MyArray.Length; i++)
+            {
+                MyArray[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 1; i < MyArray.Length - 1; i++)
+            {
+                if(MyArray[i] > MyArray[i-1] && MyArray[i] > MyArray[i + 1])
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"{count} чисел в массиве больше двух своих соседей.");
+        }
+    }
+}
