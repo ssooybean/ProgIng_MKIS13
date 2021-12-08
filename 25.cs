20705bb619b7d926d9ec0b3f83edb34895e4396e
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 20:58:15 +0700

    Add files via upload

---
+++ 25.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson4
+{
+    class Program
+    {
+        static void ChangeDigits(ref int x)
+        {
+            int a = x % 10;
+            x = (x - a) / 10;
+            x = a * 10 + x;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите двузначное число: ");
+            int num = int.Parse(Console.ReadLine());
+            ChangeDigits(ref num);
+            Console.WriteLine(num);
+        }
+    }
+}
