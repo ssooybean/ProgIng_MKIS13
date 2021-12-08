bab90725b26b4daeeb03055314536e754ccd92c1
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 19:40:19 +0700

    Add files via upload

---
+++ 17.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите русскоязычную строку:\t");
+            string s = Console.ReadLine();
+            int count = s.Split().Where(x => x[0] == 'о').Count();
+            Console.WriteLine($"\nКоличество слов, начинающихся с буквы 'о' равно {count}.\n");
+        }
+    }
+}
