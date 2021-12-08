1d49a8f868695732ec5b508588ddf65500e7b7a0
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 21:46:11 +0700

    Add files via upload

---
+++ 29.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson4
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 300; i++)
+            {
+                int coast = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        coast++;
+                    }
+                }
+                if (coast == 5)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
