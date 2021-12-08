ed8323a678c57d49e701a12580e4e0d4f9e91647
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 20:38:15 +0700

    Add files via upload

---
+++ 23.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson4
+{
+    class Program
+    {
+        static void Perimeter(int x1, int x2, int x3)
+        {
+            Console.WriteLine($"Периметр заданной фигуры {x1 + x2 + x3}");
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите длины трех сторон: ");
+            string[] lst = Console.ReadLine().Split();
+            int a = int.Parse(lst[0]);
+            int b = int.Parse(lst[1]);
+            int c = int.Parse(lst[2]);
+            Perimeter(a, b, c);
+
+        }
+    }
+}
