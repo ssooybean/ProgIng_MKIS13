b4921e05e7ca76f7dc6e241214a229f623916687
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 21:42:09 +0700

    Add files via upload

---
+++ 27.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson4
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            for (int i = 120; i <= 140; i++)
+            {
+                int coast = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        coast++;
+                    }
+                }
+                Console.WriteLine($"Число {i} имеет {coast} делителей;");
+            }
+        }
+    }
+}
