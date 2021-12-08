f123ee43282a599d4af98325d2f362122386f67d
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 20:04:32 +0700

    Add files via upload

---
+++ 21.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите русскоязычную строку:\t");
+            string s = Console.ReadLine();
+            Console.Write("Измененная строка: ");
+            Console.WriteLine(new string(s.Select(x => Char.IsUpper(x) ? Char.ToLower(x) : x).ToArray()));
+        }
+    }
+}
