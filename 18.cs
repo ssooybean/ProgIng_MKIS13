11c847e64a799391602c7b420c1735137b4d8cef
Ivakina Alina <93323532+ssooybean@users.noreply.github.com>
2021-12-08 19:52:23 +0700

    Add files via upload

---
+++ 18.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите русскоязычную строку:\t");
+            string s = Console.ReadLine();
+            int count = 0;
+            string s2 = s.Replace(":", "%");
+            Console.WriteLine($"Измененная строка: {s2}");
+            for(int i = 0; i<s.Length; i++)
+            {
+                if (s[i] != s2[i])
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"\nКоличество  замен {count}.\n");
+        }
+    }
+}
